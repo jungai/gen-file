d7549bdcdedb2d6ddefa4869b8e33da7a1dc3c0d
jungai <juworaphol125@gmail.com>
2021-12-18 15:49:14 +0700

    feat: show list command

--- main.go
+++ main.go
@@ -9,16 +9,47 @@ import (
 )
 
 func main() {
+	allConfigs, err := os.ReadDir("./templates")
+
+	if err != nil {
+		log.Fatalf("cannot read directory")
+	}
+
 	app := &cli.App{
-		Name:  "gen-file",
-		Usage: "generate file for you!!",
-		Action: func(c *cli.Context) error {
-			fmt.Println("I will do this trust me!")
-			return nil
+		Commands: []*cli.Command{
+			{
+				// TODO: add action
+				Name:    "prettier",
+				Aliases: []string{"p"},
+				Usage:   "generate preitter config file (.prettier)",
+				Action: func(c *cli.Context) error {
+					fmt.Println(c.Args().First())
+
+					return nil
+				},
+			},
+			{
+				// TODO: add action
+				Name:    "editorconfig",
+				Aliases: []string{"p"},
+				Usage:   "generate editorconfig config file (.editorconfig)",
+			},
+			{
+				Name:    "list",
+				Aliases: []string{"l"},
+				Usage:   "show all available extension",
+				Action: func(c *cli.Context) error {
+					for i := 0; i < len(allConfigs); i++ {
+						fmt.Println(allConfigs[i].Name())
+					}
+
+					return nil
+				},
+			},
 		},
 	}
 
-	err := app.Run(os.Args)
+	err = app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
